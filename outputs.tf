ad4a740a6ee7d4e44707f6d9cc9c1bc7ad067942
Carlos Jimenez <carlosmajimo@gmail.com>
2024-10-12 11:33:13 -0500

    clear outputs

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,10 @@ output "public_subnet_2_id" {
   value = module.vpc.public_subnet_2_id
 }
 
-output "ec2_public_1_ip" {
-  value = module.ec2.ec2_public_1_ip
-}
-
-output "ec2_public_2_ip" {
-  value = module.ec2.ec2_public_2_ip
-}
+# output "ec2_public_1_ip" {
+#   value = module.ec2.ec2_public_1_ip
+# }
+# 
+# output "ec2_public_2_ip" {
+#   value = module.ec2.ec2_public_2_ip
+# }
